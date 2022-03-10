f1c5fa54f21393384daa50ec9b021a8272a3474d
1327wukai <wukai091@foxmail.com>
2022-03-10 20:36:27 +0800

    添加项目文件。

---
+++ CMSWinFormsApp1/WorkerForm.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace CMSWinFormsApp1
+{
+    partial class WorkerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cMSDBDataSet = new CMSWinFormsApp1.CMSDBDataSet();
+            this.cMSDBDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tblWorkerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tblWorkerTableAdapter = new CMSWinFormsApp1.CMSDBDataSetTableAdapters.tblWorkerTableAdapter();
+            this.workerIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sqlSelectCommand1 = new System.Data.SqlClient.SqlCommand();
+            this.sqlDataAdapter1 = new System.Data.SqlClient.SqlDataAdapter();
+            this.sqlConnection1 = new System.Data.SqlClient.SqlConnection();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cMSDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cMSDBDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblWorkerBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(193, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(361, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Click on the Edit button to Load the Records\n";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(131, 295);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(438, 295);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(580, 295);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(291, 295);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 4;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.workerIDDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tblWorkerBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(109, 92);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(559, 186);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // cMSDBDataSet
+            // 
+            this.cMSDBDataSet.DataSetName = "CMSDBDataSet";
+            this.cMSDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // cMSDBDataSetBindingSource
+            // 
+            this.cMSDBDataSetBindingSource.DataSource = this.cMSDBDataSet;
+            this.cMSDBDataSetBindingSource.Position = 0;
+            // 
+            // tblWorkerBindingSource
+            // 
+            this.tblWorkerBindingSource.DataMember = "tblWorker";
+            this.tblWorkerBindingSource.DataSource = this.cMSDBDataSetBindingSource;
+            // 
+            // tblWorkerTableAdapter
+            // 
+            this.tblWorkerTableAdapter.ClearBeforeFill = true;
+            // 
+            // workerIDDataGridViewTextBoxColumn
+            // 
+            this.workerIDDataGridViewTextBoxColumn.DataPropertyName = "WorkerID";
+            this.workerIDDataGridViewTextBoxColumn.HeaderText = "WorkerID";
+            this.workerIDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.workerIDDataGridViewTextBoxColumn.Name = "workerIDDataGridViewTextBoxColumn";
+            this.workerIDDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // sqlSelectCommand1
+            // 
+            this.sqlSelectCommand1.CommandText = "SELECT workerld, Name, WorkerID FROM tblWorker";
+            this.sqlSelectCommand1.Connection = this.sqlConnection1;
+            // 
+            // sqlDataAdapter1
+            // 
+            this.sqlDataAdapter1.SelectCommand = this.sqlSelectCommand1;
+            // 
+            // sqlConnection1
+            // 
+            this.sqlConnection1.ConnectionString = "Data Source=DESKTOP-2MH1I6N;Initial Catalog=CMSDB;Integrated Security=True";
+            this.sqlConnection1.FireInfoMessageEventOnUserErrors = false;
+            // 
+            // WorkerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label1);
+            this.Name = "WorkerForm";
+            this.Text = "WorkerForm";
+            this.Load += new System.EventHandler(this.WorkerForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cMSDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cMSDBDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblWorkerBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource cMSDBDataSetBindingSource;
+        private CMSDBDataSet cMSDBDataSet;
+        private System.Windows.Forms.BindingSource tblWorkerBindingSource;
+        private CMSDBDataSetTableAdapters.tblWorkerTableAdapter tblWorkerTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn workerIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Data.SqlClient.SqlCommand sqlSelectCommand1;
+        private System.Data.SqlClient.SqlConnection sqlConnection1;
+        private System.Data.SqlClient.SqlDataAdapter sqlDataAdapter1;
+    }
+}
